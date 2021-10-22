53a2f989d9c3723a185dcb1031ca27688f13d71e
廖某人 <8010497+0x7cc@users.noreply.github.com>
2021-10-22 22:22:46 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,6 +3,8 @@
 
 int main(int argc, char* argv[]){
   int result = 0;
+#if defined(LIBVCPU_ARCH_X86_64)
   __asm__ __volatile ("mov %[from], %[to]" : [to]"=r"(result) : [from]"r"(argc));
+#endif
   return result;
 }
