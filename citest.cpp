29ade12a7a0f0e0109cff391955d8cce7334142e
liaowuping <348984539@qq.com>
2021-10-23 14:51:55 +0800

    v0.17

--- citest.cpp
+++ citest.cpp
@@ -2,7 +2,7 @@
 
 int hello(int argc, char* argv[]){
   int result = 0;
-#if defined(LIBVCPU_ARCH_X86_64)
+#if defined(__x86_64__)
   __asm__ __volatile ("mov %[from], %[to]" : [to]"=r"(result) : [from]"r"(argc));
 #endif
   return result;
